d16c52963d909ff6cb9039f525d97ee08ca41ab4
Hazari_Arshi <56993549+Hazai563@users.noreply.github.com>
2024-08-24 01:05:53 +0500

    About these files..
    I'm just uploading some files, of Easyrepo framwork, to automate the Sales module bussinces process, also one file contains Logs, screenshot capturing with Allure reprot etc... this is code only for demo purposes... thanks

---
+++ BusinessProcessFlowNextStage.cs
@@ -0,0 +1,110 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.Dynamics365.UIAutomation.Api.UCI;
+using Microsoft.Dynamics365.UIAutomation.Browser;
+using Microsoft.Dynamics365.UIAutomation.CE.Reports;
+using Microsoft.Dynamics365.UIAutomation.Sample;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Security;
+
+namespace Microsoft.Dynamics365.UIAutomation.CE.UCI
+{
+    [TestClass]
+    public class BusinessProcessFlowNextStageUCI : ExtentReportParent
+    {
+        private readonly SecureString _username = System.Configuration.ConfigurationManager.AppSettings["OnlineUsername"].ToSecureString();
+        private readonly SecureString _password = System.Configuration.ConfigurationManager.AppSettings["OnlinePassword"].ToSecureString();
+        private readonly SecureString _mfaSecretKey = System.Configuration.ConfigurationManager.AppSettings["MfaSecretKey"].ToSecureString();
+        private readonly Uri _xrmUri = new Uri(System.Configuration.ConfigurationManager.AppSettings["OnlineCrmUrl"].ToString());
+
+        [TestMethod]
+        [TestCategory("BPF")]
+        // [Priority(0)]
+        public void UCITestBusinessProcessFlowNextStage()
+        {
+            var client = new WebClient(TestSettings.Options);
+            using (var xrmApp = new XrmApp(client))
+
+                try
+                {
+
+
+                    xrmApp.OnlineLogin.Login(_xrmUri, _username, _password, _mfaSecretKey);
+
+                    xrmApp.Navigation.OpenApp(UCIAppName.Sales);
+
+                    xrmApp.Navigation.OpenSubArea("Sales", "Leads");
+
+                    //open first lead
+                    xrmApp.ThinkTime(5000);
+                    xrmApp.Grid.OpenRecord(0);
+
+                    //move to Qualify stage
+                    xrmApp.ThinkTime(5000);
+                    xrmApp.BusinessProcessFlow.SelectStage("Qualify");
+                    xrmApp.ThinkTime(3000);
+
+                    //Open and Entering the Qulify stage opetion values... 
+                    xrmApp.ThinkTime(5000);
+                    xrmApp.BusinessProcessFlow.SetValue("parentcontactid", "Hazari Arshi");
+                    xrmApp.Lookup.OpenRecord(0);
+
+
+                    xrmApp.ThinkTime(5000);
+                    xrmApp.BusinessProcessFlow.SetValue("parentaccountid", "Hazari Tech");
+                    xrmApp.Lookup.OpenRecord(0);
+
+                    xrmApp.ThinkTime(5000);
+                    xrmApp.BusinessProcessFlow.SetValue(new OptionSet { Name = "header_process_purchasetimeframe", Value = "This Quarter" });
+                    xrmApp.ThinkTime(5000);
+                    xrmApp.BusinessProcessFlow.SetValue("budgetamount", "500000");
+                    xrmApp.ThinkTime(5000);
+
+
+                    xrmApp.BusinessProcessFlow.SetValue("decisionmaker");
+                    xrmApp.ThinkTime(5000);
+                    xrmApp.BusinessProcessFlow.SetValue("description", "Testing Description");
+
+                    //closing Qualify stage
+                    xrmApp.ThinkTime(3000);
+                    xrmApp.BusinessProcessFlow.Close("Qualify");
+
+                    //qualify the lead via click button
+                    xrmApp.ThinkTime(3000);
+                    xrmApp.CommandBar.ClickCommand("Qualify");
+                    xrmApp.ThinkTime(5000);
+
+                    //moving to the BPF next stage(Develop)...
+                    xrmApp.BusinessProcessFlow.SelectStage("Develop");
+                    xrmApp.ThinkTime(5000);
+                    xrmApp.BusinessProcessFlow.SetValue("identifycompetitors");
+                    xrmApp.ThinkTime(5000);
+
+
+                    //moving to the BPF next stage
+                    xrmApp.BusinessProcessFlow.NextStage("Develop");
+                    xrmApp.ThinkTime(5000);
+                    xrmApp.BusinessProcessFlow.NextStage("Propose");
+                    xrmApp.ThinkTime(5000);
+                    xrmApp.BusinessProcessFlow.SetValue("completefinalproposal");
+                    xrmApp.ThinkTime(5000);
+
+                    //Close and Lost... with required data... 
+                    xrmApp.Dialogs.CloseOpportunity(true, 600.0855, new DateTime(), "Closing opportunity!");
+                    xrmApp.ThinkTime(5000);
+
+
+                }
+                catch (Exception e)
+                {
+
+                    TakeScreenshot(client, "UCITestBusinessProcessFlowNextStage" + CaseCount);
+                    getErrorLogs(client);
+                    markTestcaseSatus(e);
+                }
+
+        }
+    }
+}
---
+++ ExtentReportParent.cs
@@ -0,0 +1,184 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.MarkupUtils;
+using AventStack.ExtentReports.Reporter;
+using Microsoft.Dynamics365.UIAutomation.Api.UCI;
+using Microsoft.Dynamics365.UIAutomation.Browser;
+using Microsoft.Dynamics365.UIAutomation.Sample;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using static Microsoft.Dynamics365.UIAutomation.Api.Pages.ActivityFeed;
+using Status = AventStack.ExtentReports.Status;
+using TestContext = Microsoft.VisualStudio.TestTools.UnitTesting.TestContext;
+
+
+namespace Microsoft.Dynamics365.UIAutomation.CE.Reports
+{
+    [TestClass]
+    public class ExtentReportParent
+    {
+        public static ExtentReports Extent;
+        public static ExtentTest TestParent;
+        public static ExtentTest Test;
+        public string ScreenshotPath;
+        public string Base64Code;
+        public static string AssemblyName;
+        public static int CaseCount;
+        public TestContext TestContext { get; set; }
+
+
+        [AssemblyInitialize] //executes only once at start
+        public static void AssemblyInitialize(TestContext context)
+        {
+            CaseCount = 0;
+
+            AssemblyName = Assembly.GetExecutingAssembly().GetName().Name;
+            // The result is added under TestResult folder under EasyReproD365 folder
+
+            var dir = context.TestDir + "\\";
+            const string fileName = "ExtentReport.html";
+            var htmlReporter = new ExtentSparkReporter(dir + fileName);
+
+            // Add any additional contextual information
+            Extent = new ExtentReports();
+            Extent.AddSystemInfo("Browser", Enum.GetName(typeof(BrowserType), TestSettings.Options.BrowserType));
+            Extent.AddSystemInfo("Test User", System.Configuration.ConfigurationManager.AppSettings["OnlineUsername"]);
+            Extent.AddSystemInfo("D365 CE Instance", System.Configuration.ConfigurationManager.AppSettings["OnlineCrmUrl"]);
+            Extent.AttachReporter(htmlReporter);
+            context.AddResultFile(fileName);
+        }
+
+        [TestInitialize] //executes before each test
+        public void TestInitialize()
+        {
+            CaseCount++;
+            // Get unit test description attribute
+            var type = Type.GetType($"{TestContext.FullyQualifiedTestClassName}, {AssemblyName}");
+            var methodInfo = type?.GetMethod(TestContext.TestName);
+            var customAttributes = methodInfo?.GetCustomAttributes(false);
+            DescriptionAttribute desc = null;
+            if (customAttributes != null)
+            {
+                foreach (var n in customAttributes)
+                {
+                    desc = n as DescriptionAttribute;
+                    if (desc != null)
+                        break;
+                }
+            }
+
+            //Create individual test under the relevant class
+            Test = Extent.CreateTest(TestContext.TestName).AssignCategory(TestContext.FullyQualifiedTestClassName);
+        }
+
+
+        [TestCleanup] //executes after each test
+        public void TestCleanup()
+        {
+            if (Test.Status == Status.Error)
+                return;
+
+            switch (TestContext.CurrentTestOutcome)
+            {
+                case UnitTestOutcome.Error:
+                    Test.Fail("Test Failed - System Error");
+                    break;
+                case UnitTestOutcome.Passed:
+                    Test.Pass("Test Passed");
+                    break;
+                case UnitTestOutcome.Failed:
+                    Test.Fail("Test Failed");
+                    break;
+                case UnitTestOutcome.Inconclusive:
+                    Test.Fail("Test Failed - Inconclusive");
+                    break;
+                case UnitTestOutcome.Timeout:
+                    Test.Fail("Test Failed - Timeout");
+                    break;
+                case UnitTestOutcome.Aborted:
+                    Test.Skip("Test Failed - Aborted / Not Runnable");
+                    break;
+                case UnitTestOutcome.InProgress:
+                case UnitTestOutcome.Unknown:
+                default:
+                    Test.Fail("Test Failed - Unknown");
+                    break;
+            }
+
+
+            ScreenshotPath = TestContext.TestDir + "\\In\\" + TestContext.TestName + CaseCount + ".png";
+            Test.AddScreenCaptureFromPath(ScreenshotPath, TestContext.TestName + CaseCount);
+        }
+
+
+        [AssemblyCleanup] //executes only once at end of execution
+        public static void AssemblyCleanup()
+        {
+            Extent.Flush();
+        }
+
+        public void TakeScreenshot(WebClient client, string title)
+        {
+            string path = TestContext.TestDir + "\\In\\" + title + ".png";
+            client.Browser.Driver.WaitForTransaction();
+            Screenshot screenshot = ((ITakesScreenshot)client.Browser.Driver).GetScreenshot();
+            screenshot.SaveAsFile(path);
+        }
+
+        public void markTestcaseSatus(Exception e)
+        {
+            // Formats the exception details to look nice
+            var message = e.Message + Environment.NewLine + e.StackTrace.Trim();
+            var markup = MarkupHelper.CreateCodeBlock(message);
+            Test.Fail(markup);
+            throw e;
+        }
+
+        public void getErrorLogs(WebClient client)
+        {
+            client.Browser.Driver.SwitchTo().ActiveElement();
+            if (client.Browser.Driver.FindElements(By.Id("dialogErrorViewlink")).Count > 0)
+            {
+                client.Browser.Driver.ClickWhenAvailable(By.Id("dialogErrorViewlink"));
+                client.Browser.ThinkTime(2000);
+                client.Browser.Driver.ClickWhenAvailable(By.Id("dialogErrorTexttext"));
+                client.Browser.ThinkTime(4000);
+                renameLatestFile();
+            }
+        }
+
+        public void createNewFolder()
+        {
+            DateTime currentTime = DateTime.Now;
+            string folderName = currentTime.ToString("yyyy-MM-dd_HH-mm-ss");
+            string folderPath = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+            Directory.CreateDirectory(folderPath);
+        }
+
+        public void renameLatestFile()
+        {
+            string path = new BrowserOptions().DownloadsPath;
+            DirectoryInfo directory = new DirectoryInfo(path);
+
+            FileInfo latestFile = directory.GetFiles()
+                                           .OrderByDescending(f => f.CreationTime)
+                                           .FirstOrDefault();
+
+            if (latestFile != null)
+            {
+                string newFileName = Path.Combine(directory.FullName, TestContext.TestName + "_" + DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".txt");
+                latestFile.MoveTo(newFileName);
+
+                Console.WriteLine($"Renamed and changed extension of {latestFile.FullName} to {newFileName}");
+            }
+            else
+            {
+                Console.WriteLine("No files found in the folder");
+            }
+
+        }
+    }
+}
